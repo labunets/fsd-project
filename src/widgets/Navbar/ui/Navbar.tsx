071de2c17273d9df2d262a03d59c17f6576e41dd
Denis Labunets <denis@labunets.com>
2023-05-25 21:04:17 +0300

    navbar

---
+++ src/widgets/Navbar/ui/Navbar.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import {classNames} from "shared/lib/classNames/classNames";
+import cls from './Navbar.module.scss';
+import {AppLink, AppLinkTheme} from "shared/ui/AppLink/AppLink";
+
+interface NavbarProps {
+    className?: string;
+}
+
+export const Navbar = ({className}: NavbarProps) => {
+    return (
+        <div className={classNames(cls.Navbar, {}, [])}>
+            <div className={cls.links}>
+                <AppLink to={'/'} className={cls.mainLink} theme={AppLinkTheme.SECONDARY} >
+                    Главная
+                </AppLink>
+                <AppLink to={'/about'} theme={AppLinkTheme.RED}>
+                    О сайте
+                </AppLink>
+            </div>
+        </div>
+    );
+};

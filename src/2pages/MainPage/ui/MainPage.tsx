2c45840a03c1fe71570caf00055e515b62d34c4e
Denis Labunets <denis@labunets.com>
2023-11-07 12:48:57 +0200

    rm counter from main page

--- src/2pages/MainPage/ui/MainPage.tsx
+++ src/2pages/MainPage/ui/MainPage.tsx
@@ -2,14 +2,12 @@ import React from 'react';
 import { useTranslation } from 'react-i18next';
 import { Page } from '@/3widgets/Page';
 import { Text, TextSize } from '@/6shared/ui/Text';
-import { Counter } from '@/5entities/Counter';
 
 const MainPage = () => {
     const { t } = useTranslation('main');
 
     return (
         <Page data-testid="MainPage">
-            <Counter />
             <Text title={t('Main')} size={TextSize.L} />
         </Page>
     );

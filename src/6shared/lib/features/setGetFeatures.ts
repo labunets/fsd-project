d7d20ea148c0ac468489178e9994d4230534c140
Denis Labunets <denis@labunets.com>
2023-11-18 18:03:17 +0200

    fix icon color and size

--- src/6shared/lib/features/setGetFeatures.ts
+++ src/6shared/lib/features/setGetFeatures.ts
@@ -1,6 +1,6 @@
 import { FeatureFlags } from '@/6shared/types/featureFlags';
 
-let featureFlags: FeatureFlags;
+let featureFlags: FeatureFlags = {};
 
 export function setFeatureFlags(newFeatureFlags?: FeatureFlags) {
     if (newFeatureFlags) {

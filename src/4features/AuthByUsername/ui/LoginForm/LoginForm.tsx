d19ae384461db396c6a1b53c6511e77f026dfef4
Den Labunets <denis@labunets.com>
2025-09-08 17:27:53 +0300

    fix admin login placeholder

--- src/4features/AuthByUsername/ui/LoginForm/LoginForm.tsx
+++ src/4features/AuthByUsername/ui/LoginForm/LoginForm.tsx
@@ -109,7 +109,7 @@ const LoginForm = memo(({ className, onSuccess }: LoginFormProps) => {
                             type="text"
                             className={cls.input}
                             label={t('Username')}
-                            placeholder={t('John')}
+                            placeholder={t('admin')}
                             onChange={onChangeUsername}
                             autoFocus
                             value={username}
